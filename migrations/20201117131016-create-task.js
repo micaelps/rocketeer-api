ad4d9282a3cc5869bcca37b4a4c7fb64ea3348e4
micaelps <3.1416micael@gmail.com>
2020-11-17 10:22:05 -0300

    modified: owner to memberId attribute name - Task model

--- migrations/20201117131016-create-task.js
+++ migrations/20201117131016-create-task.js
@@ -11,7 +11,7 @@ module.exports = {
       name: {
         type: Sequelize.STRING
       },
-      owner: {
+      memberId: {
         type: Sequelize.INTEGER,
         references: {
           model: 'Member',
